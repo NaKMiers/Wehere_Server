93eb5d70ef897b7263af7fe40013714f65b5c935
AnK <56575187+NaKMiers@users.noreply.github.com>
2022-01-18 19:02:46 +0700

    complete render blog page

--- src/app/controllers/BlogController.js
+++ src/app/controllers/BlogController.js
@@ -1,4 +1,5 @@
 const BlogModel = require('../models/BlogModel')
+const UserModel = require('../models/UserModel')
 
 class BlogController {
    // [POST]: /blogs/post
@@ -10,6 +11,39 @@ class BlogController {
          const blogStatus = BlogModel({ ...data, userId })
          await blogStatus.save()
          res.status(200).json('BlogStatus has been created.')
+      } catch (err) {
+         res.status(500).json(err)
+      }
+   }
+
+   // [GET]: /blogs/get-blogs-newfeed
+   getBlogsNewfeed = async function (req, res) {
+      console.log('getBlogNewFeed')
+      const userId = req.user._id
+      console.log('userId: ', userId)
+      try {
+         // get a friendList
+         const curUser = await UserModel.findById(userId)
+
+         // get users from friendList
+         let friends = await UserModel.find({ _id: { $in: curUser.friends } })
+         friends = friends.concat(curUser)
+
+         // get blogs with each friend from friendList and from my blogs
+         const originBlogs = await BlogModel.find({
+            $or: [{ userId: { $in: curUser.friends } }, { userId: userId }],
+         })
+
+         const completedBlogs = originBlogs.map(b => {
+            let friendMatch = friends.find(f => {
+               return f._id.toString() === b.userId
+            })
+
+            let { createdAt, password, ...other } = friendMatch._doc
+            return { blog: b, author: other }
+         })
+
+         res.status(200).json(completedBlogs)
       } catch (err) {
          res.status(500).json(err)
       }
--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const BlogController = require('../app/controllers/BlogController')
 const authMiddleware = require('../app/middlewares/authMiddleware')
 
+router.get('/get-blogs-newfeed', authMiddleware, BlogController.getBlogsNewfeed)
 router.post('/post', authMiddleware, BlogController.postBlogStatus)
 
 module.exports = router
