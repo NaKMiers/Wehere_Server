f7f2c1233e92161b4e1ff81741430d77daab47b7
AnK <56575187+NaKMiers@users.noreply.github.com>
2022-02-09 17:47:57 +0700

    asdsd

--- src/app/controllers/ImageController.js
+++ src/app/controllers/ImageController.js
@@ -51,6 +51,7 @@ class ImageController {
       console.log('getImagesNewfeed')
 
       const userId = req.user._id
+      console.log('userId: ', userId)
       try {
          // get a friendList
          const curUser = await UserModel.findById(userId)
--- src/app/controllers/VideoController.js
+++ src/app/controllers/VideoController.js
@@ -50,6 +50,7 @@ class VideoController {
       console.log('getVideosNewfeed')
 
       const userId = req.user._id
+      console.log('userId: ', userId)
       try {
          // get a friendList
          const curUser = await UserModel.findById(userId)
