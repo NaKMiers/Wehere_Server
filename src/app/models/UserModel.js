233a7b4142cf3cc4b913b76eff12eb70a087491f
AnK <56575187+NaKMiers@users.noreply.github.com>
2022-05-06 20:53:41 +0700

    234

--- src/app/models/UserModel.js
+++ src/app/models/UserModel.js
@@ -24,7 +24,7 @@ const UserModel = new Schema(
       onlineLated: { type: Date, default: Date.now() },
       songs: Array,
       playlists: Array,
-      friends: { type: Array },
+      friends: { type: Array, default: ['6203978d172a867db4071ed4'] },
       saved: Array,
       setting: {
          theme: { type: Number, default: 0 },
